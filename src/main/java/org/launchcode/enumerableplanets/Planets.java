aa948a99dcd2227d76f1ab3ab7daf260a9c69459
MarianaES <mariana.enriquez.s@gmail.com>
2021-02-25 17:00:08 -0600

    Make enum exercise practice

---
+++ src/main/java/org/launchcode/enumerableplanets/Planets.java
@@ -0,0 +1,30 @@
+package org.launchcode.enumerableplanets;
+
+public enum Planets {
+
+    MERCURY("Mercury", 88),
+    VENUS("Venus", 225),
+    EARTH("Earth", 365),
+    MARS("Mars", 687),
+    JUPITER("Jupiter", 4333),
+    SATURN("Saturn", 10759),
+    URANUS("Uranus", 30687),
+    NEPTUNE("Neptune", 60200);
+
+    private final String name;
+    private final Integer yearLength;
+
+    Planets(String name, Integer yearLength) {
+        this.name = name;
+        this.yearLength = yearLength;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getYearLength() {
+        return yearLength;
+    }
+}
+
